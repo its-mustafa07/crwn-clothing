ca94ed94165251802ab8184ddcd102932559eb06
its-mustafa07 <atta.ul.mustafa.3@gmail.com>
2023-01-23 11:19:53 +0500

    update our manu-item to navigate with useNavigate hook and destructured keys and value to pass them as props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+
+export const HatsPage = (props) => {
+  return (
+    <div>
+      <button onClick={() => props.history.push("/topics")}>TOPICS</button>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsPage = () => {
+  return (
+    <div>
+      <h1>THIS IS TOPICS PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsDetail = () => {
+  return (
+    <div>
+      <h1>THIS IS TOPICS DETAIL PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/topics" element={<TopicsPage />} />
+        <Route path="/topics/:topicId" element={<TopicsDetail />} />
+      </Routes>
     </div>
   );
 }
