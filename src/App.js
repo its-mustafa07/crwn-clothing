de888082006ced5dd7bd79d0ebdf0cc27c65d51a
its-mustafa07 <atta.ul.mustafa.3@gmail.com>
2023-01-24 13:05:41 +0500

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,39 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
-export const HatsPage = (props) => {
-  return (
-    <div>
-      <button onClick={() => props.history.push("/topics")}>TOPICS</button>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
-
-const TopicsPage = () => {
-  return (
-    <div>
-      <h1>THIS IS TOPICS PAGE</h1>
-    </div>
-  );
-};
-
-const TopicsDetail = () => {
-  return (
-    <div>
-      <h1>THIS IS TOPICS DETAIL PAGE</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
-        <Route path="/topics" element={<TopicsPage />} />
-        <Route path="/topics/:topicId" element={<TopicsDetail />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
